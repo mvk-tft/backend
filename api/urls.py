489522b66f5f5184e086b679fc1fa32503d97176
ateo <ateo@kth.se>
2020-03-17 18:27:43 +0100

    added urlpatterns for trucks, cargo and companies

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,15 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
+
+# url patterns for trucks, cargo and companies
+urlpatterns = [
+    path('truck/', views.TruckList.as_view()),
+    path('truck/<int:pk>/', views.TruckDetail.as_view()),
+    path('cargo/', views.CargoList.as_view()),
+    path('cargo/<int:pk>/', views.CargoDetail.as_view()),
+    path('company/', views.CompanyList.as_view()),
+    path('company/<int:pk>/', views.CompanyDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
