aff02f5dfe7fd071feb5d44add5014763c0ba84c
ggerholm <ggerholm@kth.se>
2020-03-23 17:22:18 +0100

    Added URL-names.

--- api/urls.py
+++ api/urls.py
@@ -5,14 +5,14 @@ from api.views import ShipmentList, TruckList, CargoList, TruckDetail, CargoDeta
     ShipmentDetail
 
 urlpatterns = [
-    path('truck/', TruckList.as_view()),
-    path('truck/<int:pk>/', TruckDetail.as_view()),
-    path('cargo/', CargoList.as_view()),
-    path('cargo/<int:pk>/', CargoDetail.as_view()),
-    path('company/', CompanyList.as_view()),
-    path('company/<int:pk>/', CompanyDetail.as_view()),
-    path('shipment/', ShipmentList.as_view()),
-    path('shipment/<int:pk>/', ShipmentDetail.as_view()),
+    path('truck/', TruckList.as_view(), name='truck_list'),
+    path('truck/<int:pk>/', TruckDetail.as_view(), name='truck_detail'),
+    path('cargo/', CargoList.as_view(), name='cargo_list'),
+    path('cargo/<int:pk>/', CargoDetail.as_view(), name='cargo_detail'),
+    path('company/', CompanyList.as_view(), name='company_list'),
+    path('company/<int:pk>/', CompanyDetail.as_view(), name='company_detail'),
+    path('shipment/', ShipmentList.as_view(), name='shipment_list'),
+    path('shipment/<int:pk>/', ShipmentDetail.as_view(), name='shipment_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
